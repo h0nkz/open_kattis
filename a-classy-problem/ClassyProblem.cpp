f73be362644625da897d8bba5785911e00566aac
h0nkz <102213145+h0nkz@users.noreply.github.com>
2025-06-22 21:09:13 +0200

    Working solution for A classy Problem

--- a-classy-problem/ClassyProblem.cpp
+++ a-classy-problem/ClassyProblem.cpp
@@ -1,18 +1,52 @@
 #include <iostream>
 #include <stack>
 #include <string>
-#include <vector>
 #include <cmath>
-
+#include <unordered_map>
+#include <map>
+#include <vector>
+#include <sstream>
 /*
     https://open.kattis.com/problems/classy
 */
+
+std::unordered_map<std::string, long> classValueMap{};
 
 struct Person
 {
     std::string name;
     long value;
+
+    /*
+        Since the map sorts ascending, we make the operator
+        oposite so that it gets sorted descending for the value,
+        but keep less than for the lexicographical comparison
+    */
+    bool operator<(const Person& other) const
+    {
+        if(value == other.value)
+        {
+            return name < other.name;
+        }
+
+        return value > other.value;
+    }
 };
+
+int get_char_value(char c)
+{
+    switch (c)
+    {
+    case 'u':
+        return 3;
+    case 'm':
+        return 2;
+    case 'l':
+        return 1;
+    }
+
+    return -1;
+}
 
 long parse_class_value(std::string classString)
 {
@@ -37,36 +71,87 @@ long parse_class_value(std::string classString)
     int exp = 10 - classStack.size(); // Get the exponent for modulus by subracting size
     baseValue = baseValue % (long)pow(10, exp); // Remainder is the values that should be default
 
-    for(int i = 1; i <= classStack.size(); i++)
+    int stackSize = classStack.size();
+    for(int i = 1; i <= stackSize; i++)
     {
-
+        baseValue += get_char_value(classStack.top()) * pow(10, 10 - i);
+        classStack.pop();
     }
 
+    classValueMap.insert_or_assign(classString, baseValue);
 
+    return baseValue;
+}
 
+std::vector<std::string> split(const char *str, char c = ' ')
+{
+    std::vector<std::string> result;
 
+    do
+    {
+        const char *begin = str;
+
+        while(*str != c && *str)
+            str++;
+
+        result.push_back(std::string(begin, str));
+    } while (0 != *str++);
+
+    return result;
 }
 
 int main()
 {
     int cases, persons;
-
-    std::cin >> cases;
+    std::string casesString, personsString;
+    std::string breakLine(30, '=');
+    std::getline(std::cin, casesString);
+    cases = stoi(casesString);
+    std::vector<std::map<Person, std::string>> mapVector{};
 
     for(int i = 0; i < cases; i++)
     {
-        std::cin >> persons;
-        std::vector<Person> personVector{};
+        std::getline(std::cin, personsString);
+        persons = stoi(personsString);
+        std::map<Person, std::string> personMap;
+        std::string temp{};
         for(int j = 0; j < persons; j++)
         {
+            std::getline(std::cin, temp);
+            std::stringstream ss(temp);
+            std::vector<std::string> tokens;
+            std::string token;
+
+            while (std::getline(ss, token, ' ')) { // using space as delimiter
+                tokens.push_back(token);
+            }
+            
             Person person{};
             std::string classString{};
-            std::cin >> person.name;
-            std::cin >> classString;
-            person.value = parse_class_value(classString);
+            person.name = tokens[0].erase(tokens[0].size() - 1);
+            classString = tokens[1];
 
+            if(auto search = classValueMap.find(classString); search != classValueMap.end())
+            {
+                person.value = search->second;
+            }
+            else
+            {
+                person.value = parse_class_value(classString);
+            }
 
+            personMap.emplace(std::make_pair(person, person.name));
         }
+        mapVector.push_back(personMap);
+    }
 
+    for(auto ptr = mapVector.begin(); ptr != mapVector.end(); ptr++)
+    {
+        for(const auto& [key,value] : *ptr)
+        {
+            std::cout << value << std::endl;
+        }
+        
+        std::cout << breakLine << std::endl;
     }
 }
