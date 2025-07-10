31e6f2756671fa4e459a1dcb021692292e91d8e8
h0nkz <102213145+h0nkz@users.noreply.github.com>
2025-07-10 22:23:31 +0200

    Add start of feast4cats solution

--- a-feast-for-cats/feast4cats.py
+++ a-feast-for-cats/feast4cats.py
@@ -1,2 +1,45 @@
 
 # https://open.kattis.com/problems/cats 
+
+class Edge:
+  def __init__(self, edgeFrom, edgeTo, weight):
+    self.edgeFrom = edgeFrom
+    self.edgeTo = edgeTo
+    self.weight = weight
+
+def sortFunc(e):
+   return e.weight
+
+
+testCases = input()
+milkAndCats = input()
+milkAndCats = milkAndCats.split()
+
+milk = float(milkAndCats[0])
+cats = float(milkAndCats[1])
+
+testCases = float(testCases)
+
+
+edgeList = []
+
+
+edges = (cats * (cats - 1)) / 2
+
+for i in range(edges) :
+    edge = input()
+    edge = edge.split()
+    edgeList.insert(Edge(float(edge[0]), float(edge[1]), float(edge[2])))
+    edgeList.insert(Edge(float(edge[1]), float(edge[0]), float(edge[2])))
+
+edgeList.sort(key=sortFunc)
+
+
+
+
+
+
+
+
+
+
